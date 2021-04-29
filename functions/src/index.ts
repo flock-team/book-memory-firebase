cc5ee0aa260686b273dbd2e756d8a570e5349535
welove-t <tlove.tky@gmail.com>
2021-04-29 16:02:29 +0900

    fix(deleteUser):ユーザー削除ファンクション追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp(functions.config().firebase);
+export * from './deleteUser';
 export * from './deleteUser-functions';
